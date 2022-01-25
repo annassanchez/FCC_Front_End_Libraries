a723798264f363182eeaf570230eac588ad738cf
anna s <annassanchez@gmail.com>
2022-01-25 10:50:20 +0100

    03_addCommentsInJSX added

---
+++ 04_react/03_addCommentsInJSX.jsx
@@ -0,0 +1,14 @@
+//jsx is a syntax that gets compiled into valid js
+//sometimes, for readability, you might need to add comments to your code
+//like most programming languages, jsx has its own way to do this
+//to put comments inside jsx, you use the syntax "{/* */}" to wrap around the comment text
+//
+//the code editor has a jsx element similar to what you created in the last challenge
+//add a comment somewhere within the provided "div" element, without modifying the existing "h1" or "p" elements
+const JSX = (
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+      {/*hello*/}
+    </div>
+  );  
